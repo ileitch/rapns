9cae5cfee2bebf9bb4f39cb9b6c0e2eab9e98c46
Forrest Ye <afu@forresty.com>
2012-01-25 17:44:50 +0800

    binary notification validator error message fix

--- lib/rapns/binary_notification_validator.rb
+++ lib/rapns/binary_notification_validator.rb
@@ -3,7 +3,7 @@ module Rapns
 
     def validate(record)
       if record.payload_size > 256
-        record.errors[:base] << "APN notification cannot be larger than 256 bytes. Try condensing your alert and device attributes."
+        record.errors[:base] << "APN notification payload cannot be larger than 256 bytes. Try condensing your alert attribute."
       end
     end
   end

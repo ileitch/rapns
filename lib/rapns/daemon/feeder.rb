0e4436de815df32a22fe23164c64972da1283b03
dei79 <dirk.eisenberg@gmail.com>
2012-08-12 16:39:19 +0200

    Added support for remote signal so it's possible to notify the daemon for reloading the application directly in your rails app. Just use
    Rapns::RemoteSignal.push :key => :hup

--- lib/rapns/daemon/feeder.rb
+++ lib/rapns/daemon/feeder.rb
@@ -11,6 +11,7 @@ module Rapns
       def self.start(poll)
         loop do
           break if @stop
+          process_pending_signals
           enqueue_notifications
           interruptible_sleep poll
         end
@@ -30,6 +31,24 @@ module Rapns
             batch_size = Rapns::Daemon.config.batch_size
             Rapns::Notification.ready_for_delivery.find_each(:batch_size => batch_size) do |notification|
               Rapns::Daemon::AppRunner.deliver(notification) if ready_apps.include?(notification.app)
+            end
+          end
+        rescue StandardError => e
+          Rapns::Daemon.logger.error(e)
+        end
+      end
+
+      def self.process_pending_signals
+        begin
+          with_database_reconnect_and_retry do
+            loop do
+              signal = Rapns::RemoteSignal.pop
+              break if signal.nil?
+
+              case signal.key.to_sym
+                when :hup then Rapns::Daemon::AppRunner.sync
+                else Rapns::Daemon.logger.info("[RemoteSignal] Unknown singal received #{signal.key} (ignoring) ")
+              end
             end
           end
         rescue StandardError => e
